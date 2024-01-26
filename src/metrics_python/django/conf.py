440ba4d5cb3d73d44c4acdd52994c32a55175e19
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2024-01-26 10:53:21 +0100

    Add middleware to count queries and keep track og query durations

---
+++ src/metrics_python/django/conf.py
@@ -0,0 +1,16 @@
+from django.conf import settings as django_settings
+
+
+class Settings:
+    @property
+    def PRINT_DUPLICATE_QUERIES(self) -> bool:
+        return bool(
+            getattr(
+                django_settings,
+                "METRICS_PYTHON_PRINT_DUPLICATE_QUERIES",
+                False,
+            )
+        )
+
+
+settings = Settings()
